cfa8d5820dfbc8af19d99a9b7551785e748ea26f
DENG Zhihong <dengzhihong3@gmail.com>
2015-02-25 18:30:40 +0800

    add some code
    So much work to do

--- src/Cluster/kmeans.py
+++ src/Cluster/kmeans.py
@@ -1,1 +1,87 @@
 __author__ = 'dengzhihong'
+
+from src.Cluster.base import *
+import numpy as np
+
+class KMeans(CommonMethod):
+    @staticmethod
+    def clusterAssignment(data, Mean):
+        D = data.shape[1]
+        K = Mean.shape[0]
+        N = data.shape[0]
+        min = np.inf
+        Z = np.zeros((N, 1))
+        index = -1
+        for i in range(N):
+            for k in range(K):
+                norm = KMeans.getSquareNorm(data[i], Mean[k])
+                #print norm
+                if(norm < min):
+                    index = k
+                    min = norm
+            min = np.inf
+            Z[i] = index
+            index = -1
+        return Z
+
+    @staticmethod
+    def estimateCenter(data, label, K):
+        D = data.shape[1]
+        Mean = np.zeros((K,D))
+        for j in range(K):
+            DataInClusterJ = data[np.nonzero(label[:,0] == j)]
+            Mean[j,:] = np.mean(DataInClusterJ, axis=0)
+        return Mean
+
+    @staticmethod
+    def genRandMean(data, K):
+        D = data.shape[1]
+        RandMean = np.mat(np.zeros((K, D)))
+        for i in range(D):
+            LowValue = np.min(data[:,i])
+            RangeValue = float(np.max(data[:,i]) - LowValue)
+            RandMean[:,i] = LowValue + RangeValue * np.random.rand(K, 1)
+        return RandMean
+
+    @staticmethod
+    def isVectorConverge(v1, v2):
+        threshold = 0.0001
+        if((abs(v1 - v2) > threshold).all() == False):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def runKmeans(data, K):
+        D = data.shape[1]
+        N = data.shape[0]
+        Z = np.zeros((N, 1))
+        Mean = KMeans.genRandMean(data, K)
+        print 'Initial Mean: '
+        print Mean
+        count = 0
+        Z_Old = np.zeros((N,1))
+        while(True):
+            print '++++++++++++++++++++++++++++++++'
+            print count
+
+            Z = KMeans.clusterAssignment(data, Mean)
+            Mean = KMeans.estimateCenter(data, Z, K)
+
+            print Mean
+            count +=1
+            if(KMeans.isVectorConverge(Z, Z_Old)):
+                print 'Converge'
+                print Z.transpose()
+                break
+        return Z.reshape(-1).astype(int).tolist()
+
+    @staticmethod
+    def showDiagramInCluster(X, Z, title="Kmeans"):
+        CommonMethod.showDiagramInCluster(X, Z, title)
+
+    @staticmethod
+    def testWithKmeans(data, title, K):
+        X = CommonMethod.preprocessRawData(data)
+        Z = KMeans.runKmeans(X, K)
+        KMeans.showDiagramInCluster(X, Z)
--- src/Demo/test.py
+++ src/Demo/test.py
@@ -1,1 +1,36 @@
 __author__ = 'dengzhihong'
+
+from src.Cluster.base import *
+from src.Cluster.kmeans import *
+
+if __name__ == '__main__':
+    dataA_X = str(open('./data/cluster_data_dataA_X.txt', 'r').read()).split()
+    dataA_Y = str(open('./data/cluster_data_dataA_Y.txt', 'r').read()).split()
+    dataB_X = str(open('./data/cluster_data_dataB_X.txt', 'r').read()).split()
+    dataB_Y = str(open('./data/cluster_data_dataB_Y.txt', 'r').read()).split()
+    dataC_X = str(open('./data/cluster_data_dataC_X.txt', 'r').read()).split()
+    dataC_Y = str(open('./data/cluster_data_dataC_Y.txt', 'r').read()).split()
+
+    #CommonMethod.drawLabeledDiagram(dataA_X, dataA_Y, 'Data A')
+    #CommonMethod.drawLabeledDiagram(dataB_X, dataB_Y, 'Data B')
+    #CommonMethod.drawLabeledDiagram(dataC_X, dataC_Y, 'Data C')
+
+    #Kmeans-----------------------------------------------------------------
+
+    KMeans.testWithKmeans(dataA_X, 'Data A', 4)
+    #testWithKmeans(dataA_X, 'Data A')
+    #testWithKmeans(dataB_X, 'Data B')
+    #testWithKmeans(dataC_X, 'Data C')
+
+    #EM---------------------------------------------------------------------
+
+    #testWithEM(dataA_X, 'Data A')
+    #testWithEM(dataB_X, 'Data B')
+    #testWithEM(dataC_X, 'Data C')
+
+    #Mean-shift-------------------------------------------------------------
+    #testWithMeanShift(dataA_X, 'Data A')
+    #testWithMeanShift(dataB_X, 'Data B')
+    #testWithMeanShift(dataC_X, 'Data C')
+
+
